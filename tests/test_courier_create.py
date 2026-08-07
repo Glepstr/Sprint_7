c79862d25bb04a4fc07669770484d2a404900a93
Glep Glepovski <pautov.glebster@yandex.ru>
2026-08-07 10:27:10 +0300

    new test + changes in test_courier_create

--- tests/test_courier_create.py
+++ tests/test_courier_create.py
@@ -1,17 +1,16 @@
 import pytest
 import allure
-import requests
-from data import ERROR_MESSAGES, COURIER_ENDPOINT, BASE_URL
-from helpers import generate_random_string, register_new_courier_and_return_login_password
+from helpers import generate_random_string
 from api.courier_api import CourierAPI
+from data import ERROR_MESSAGES
 
 @allure.feature('Курьер')
 @allure.story('Создание курьера')
 class TestCourierCreate:
     
     @allure.title('Курьера можно создать')
-    def test_create_courier_success(self):
-        login, password, first_name = register_new_courier_and_return_login_password()
+    def test_create_courier_success(self, create_courier):
+        login, password, first_name = create_courier
         assert login is not None
         assert password is not None
         assert first_name is not None
@@ -31,33 +30,6 @@ class TestCourierCreate:
         assert response.json().get('message') == ERROR_MESSAGES['COURIER_CREATE_EXIST']
     
     @allure.title('Нужно передать все обязательные поля для создания курьера')
-    def test_create_courier_missing_login(self):
-        payload = {
-            "password": generate_random_string(10),
-            "firstName": generate_random_string(10)
-        }
-        
-        response = CourierAPI.create_courier(payload)
-        assert response.status_code == 400
-        assert response.json().get('message') == ERROR_MESSAGES['COURIER_CREATE_MISSING_FIELD']
-    
-    @allure.title('Успешный запрос возвращает правильный код ответа и тело')
-    def test_create_courier_success_response(self):
-        login = generate_random_string(10)
-        password = generate_random_string(10)
-        first_name = generate_random_string(10)
-        
-        payload = {
-            "login": login,
-            "password": password,
-            "firstName": first_name
-        }
-        
-        response = CourierAPI.create_courier(payload)
-        assert response.status_code == 201
-        assert response.json() == {"ok": True}
-    
-    @allure.title('Возвращается ошибка при отсутствии обязательных полей (login или password)')
     @pytest.mark.parametrize('missing_field', ['login', 'password'])
     def test_create_courier_missing_required_fields(self, missing_field):
         payload = {
@@ -70,12 +42,28 @@ class TestCourierCreate:
         response = CourierAPI.create_courier(payload)
         assert response.status_code == 400
         assert response.json().get('message') == ERROR_MESSAGES['COURIER_CREATE_MISSING_FIELD']
-
+    
     @allure.title('Можно создать курьера без поля firstName')
     def test_create_courier_without_firstname(self):
         payload = {
             "login": generate_random_string(10),
             "password": generate_random_string(10)
+        }
+        
+        response = CourierAPI.create_courier(payload)
+        assert response.status_code == 201
+        assert response.json() == {"ok": True}
+    
+    @allure.title('Успешный запрос возвращает правильный код ответа и тело')
+    def test_create_courier_success_response(self):
+        login = generate_random_string(10)
+        password = generate_random_string(10)
+        first_name = generate_random_string(10)
+        
+        payload = {
+            "login": login,
+            "password": password,
+            "firstName": first_name
         }
         
         response = CourierAPI.create_courier(payload)
