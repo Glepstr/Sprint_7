ff392273d856a41e054a0fdfa5fff744f1bea902
Glep Glepovski <pautov.glebster@yandex.ru>
2026-08-07 10:13:38 +0300

    add 2 methods to courier api

--- api/courier_api.py
+++ api/courier_api.py
@@ -1,6 +1,6 @@
 import requests
 import allure
-from data import BASE_URL, COURIER_ENDPOINT, COURIER_LOGIN_ENDPOINT
+from data import BASE_URL, COURIER_ENDPOINT, COURIER_LOGIN_ENDPOINT, COURIER_DELETE_ENDPOINT
 
 class CourierAPI:
     
@@ -17,3 +17,20 @@ class CourierAPI:
         with allure.step(f"Логин курьера с данными: {payload}"):
             response = requests.post(f'{BASE_URL}{COURIER_LOGIN_ENDPOINT}', data=payload)
             return response
+
+    @staticmethod
+    def delete_courier(courier_id):
+        """Удаление курьера"""
+        with allure.step(f"Удаление курьера с id: {courier_id}"):
+            response = requests.delete(f'{BASE_URL}{COURIER_DELETE_ENDPOINT.format(courier_id)}')
+            return response
+    
+    @staticmethod
+    def get_courier_id(login, password):
+        """Получение id курьера по логину и паролю"""
+        with allure.step(f"Получение id курьера"):
+            payload = {"login": login, "password": password}
+            response = requests.post(f'{BASE_URL}{COURIER_LOGIN_ENDPOINT}', data=payload)
+            if response.status_code == 200:
+                return response.json().get('id')
+            return None
