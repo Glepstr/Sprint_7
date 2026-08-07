b3f9395f8474af8d14d615aeaed4b5ffda35dee3
Glep Glepovski <pautov.glebster@yandex.ru>
2026-08-07 10:22:49 +0300

    add new static method to order api file

--- api/order_api.py
+++ api/order_api.py
@@ -7,7 +7,7 @@ class OrderAPI:
     @staticmethod
     def create_order(payload):
         """Создание заказа"""
-        with allure.step(f"Создание заказа с данными: {payload}"):
+        with allure.step(f"Создание заказа"):
             response = requests.post(f'{BASE_URL}{ORDER_ENDPOINT}', json=payload)
             return response
     
@@ -23,4 +23,13 @@ class OrderAPI:
         """Получение заказа по треку"""
         with allure.step(f"Получение заказа по треку: {track}"):
             response = requests.get(f'{BASE_URL}{ORDER_TRACK_ENDPOINT}', params={'t': track})
+            return response
+
+
+    @staticmethod
+    def accept_order(order_id, courier_id):
+        """Принятие заказа курьером"""
+        with allure.step(f"Принятие заказа {order_id} курьером {courier_id}"):
+            url = f'{BASE_URL}{ORDER_ACCEPT_ENDPOINT.format(order_id)}'
+            response = requests.put(url, params={'courierId': courier_id})
             return response
