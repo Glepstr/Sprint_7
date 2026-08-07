e2a0e960f4c66bb545433da04629ed1ce50913b1
Glep Glepovski <pautov.glebster@yandex.ru>
2026-08-07 10:12:09 +0300

    update data in data.py

--- data.py
+++ data.py
@@ -1,22 +1,30 @@
-import allure
-
+# data.py
 BASE_URL = 'https://qa-scooter.praktikum-services.ru'
 
-# Эндпоинты для курьеров
+# Эндпоинты
 COURIER_ENDPOINT = '/api/v1/courier'
 COURIER_LOGIN_ENDPOINT = '/api/v1/courier/login'
 COURIER_DELETE_ENDPOINT = '/api/v1/courier/{}'
-
-# Эндпоинты для заказов
 ORDER_ENDPOINT = '/api/v1/orders'
 ORDER_TRACK_ENDPOINT = '/api/v1/orders/track'
 ORDER_ACCEPT_ENDPOINT = '/api/v1/orders/accept/{}'
 
-# Цвета для заказа
+# Цвета
 COLORS = {
     'BLACK': 'BLACK',
-    'GREY': 'GREY',
-    'NONE': None
+    'GREY': 'GREY'
+}
+
+# Базовые данные для заказа
+ORDER_DATA = {
+    "firstName": "Test",
+    "lastName": "User",
+    "address": "Test Address 123",
+    "metroStation": 4,
+    "phone": "+79999999999",
+    "rentTime": 5,
+    "deliveryDate": "2024-12-31",
+    "comment": "Test comment"
 }
 
 # Сообщения об ошибках
@@ -25,11 +33,8 @@ ERROR_MESSAGES = {
     'COURIER_CREATE_EXIST': 'Этот логин уже используется. Попробуйте другой.',
     'COURIER_LOGIN_MISSING_FIELD': 'Недостаточно данных для входа',
     'COURIER_LOGIN_INVALID': 'Учетная запись не найдена',
-    'COURIER_DELETE_MISSING_ID': 'Недостаточно данных для удаления курьера',
     'COURIER_DELETE_NOT_FOUND': 'Курьера с таким id нет.',
-    'ORDER_ACCEPT_MISSING_COURIER': 'Недостаточно данных для принятия заказа',
     'ORDER_ACCEPT_INVALID_COURIER': 'Курьера с таким id не существует',
-    'ORDER_ACCEPT_INVALID_ORDER': 'Заказа с таким id не существует',
     'ORDER_TRACK_MISSING_NUMBER': 'Недостаточно данных для поиска',
     'ORDER_TRACK_NOT_FOUND': 'Заказ не найден'
 }
