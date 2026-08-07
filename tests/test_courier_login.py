95bb3225cb067d1ed784f67a3e343d62b1e697ab
Glep Glepovski <pautov.glebster@yandex.ru>
2026-08-07 10:28:42 +0300

    add asserts

--- tests/test_courier_login.py
+++ tests/test_courier_login.py
@@ -1,8 +1,8 @@
 import pytest
 import allure
-from data import ERROR_MESSAGES
-from helpers import generate_random_string, get_courier_id
+from helpers import generate_random_string
 from api.courier_api import CourierAPI
+from data import ERROR_MESSAGES
 
 @allure.feature('Курьер')
 @allure.story('Логин курьера')
@@ -22,15 +22,17 @@ class TestCourierLogin:
         assert 'id' in response.json()
     
     @allure.title('Для авторизации нужно передать все обязательные поля')
-    def test_courier_login_missing_field(self):
+    @pytest.mark.parametrize('missing_field', ['login', 'password'])
+    def test_courier_login_missing_field(self, missing_field):
         payload = {
-            "login": generate_random_string(10)
+            "login": generate_random_string(10),
+            "password": generate_random_string(10)
         }
+        payload.pop(missing_field)
         
         response = CourierAPI.login_courier(payload)
-        assert response.status_code in [400, 504]
-        if response.status_code == 400:
-            assert response.json().get('message') == ERROR_MESSAGES['COURIER_LOGIN_MISSING_FIELD']
+        assert response.status_code == 400
+        assert response.json().get('message') == ERROR_MESSAGES['COURIER_LOGIN_MISSING_FIELD']
     
     @allure.title('Система вернёт ошибку при неправильном логине или пароле')
     def test_courier_login_invalid_credentials(self, create_courier):
@@ -44,18 +46,6 @@ class TestCourierLogin:
         response = CourierAPI.login_courier(payload)
         assert response.status_code == 404
         assert response.json().get('message') == ERROR_MESSAGES['COURIER_LOGIN_INVALID']
-    
-    @allure.title('Ошибка при отсутствии какого-либо поля')
-    @pytest.mark.parametrize('missing_field', ['login', 'password'])
-    def test_courier_login_missing_any_field(self, missing_field):
-        payload = {
-            "login": generate_random_string(10),
-            "password": generate_random_string(10)
-        }
-        payload.pop(missing_field)
-        
-        response = CourierAPI.login_courier(payload)
-        assert response.status_code in [400, 504]
     
     @allure.title('Ошибка при авторизации под несуществующим пользователем')
     def test_courier_login_nonexistent(self):
