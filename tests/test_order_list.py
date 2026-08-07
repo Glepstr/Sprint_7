6aebe33a45a1464a4ceb44182d3860debcbc1a12
Glep Glepovski <pautov.glebster@yandex.ru>
2026-08-07 10:32:55 +0300

    removed the excess method

--- tests/test_order_list.py
+++ tests/test_order_list.py
@@ -12,14 +12,3 @@ class TestOrderList:
         assert isinstance(response.json(), dict)
         assert 'orders' in response.json()
         assert isinstance(response.json()['orders'], list)
-    
-    @allure.title('В ответе есть информация о заказах')
-    def test_order_list_contains_order_data(self):
-        response = OrderAPI.get_orders()
-        assert response.status_code == 200
-        orders = response.json()['orders']
-        if len(orders) > 0:
-            order = orders[0]
-            assert 'id' in order
-            assert 'firstName' in order
-            assert 'lastName' in order
